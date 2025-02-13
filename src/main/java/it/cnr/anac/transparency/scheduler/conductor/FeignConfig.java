3cefb0e05dc93a5b4c03f1abd382da25de0405ea
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2025-02-13 09:57:10 +0100

    Inseriti log del feign.

---
+++ src/main/java/it/cnr/anac/transparency/scheduler/conductor/FeignConfig.java
@@ -0,0 +1,14 @@
+package it.cnr.anac.transparency.scheduler.conductor;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+  @Bean
+  Logger.Level feignLoggerLevel() {
+    return Logger.Level.FULL;
+  }
+}
